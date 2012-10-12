bc1c91e5f5232a293b7f4b3d4d779fe0ea769cec
Brian Fife <bdfife@gmail.com>
2012-10-12 13:06:49 -0400

    Added support for artist d/l and caching. The final implementation may not cache, but doing this for now. Not checking in the JSON files just yet for all the genres.

--- artistmap.py
+++ artistmap.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from secrets import apikey, sign
+
+import requests
 import os
 
 import json
@@ -15,9 +18,14 @@ def genre_list():
     f = open('genre_cache.json', 'r')
     genres = json.load(f)
     f.close()
-    genres = genres.get('genres')
     return render_template('genres.html', genres=genres)
 
+@app.route('/artists/genre/<id>')
+def get_artist_by_genre(id):
+    f = open ('genre_' + str(id) + '.json', 'r')
+    artists = json.load(f)
+    f.close()
+    return str(artists)
 
 if __name__ == '__main__':
     app.debug = True
--- cache_artists.py
+++ cache_artists.py
@@ -13,11 +13,44 @@ def get_genres():
     base_url = "http://api.rovicorp.com/data/v1.1/descriptor/musicgenres" 
     r = requests.get(base_url, params=req_vars)
     data = r.json
+    data = data.get('genres')
     
     f = open("genre_cache.json", 'w')
     json.dump(data, f, indent=4)
     f.close()
 
-if __name__ == "__main__":
-    print get_genres()
+def get_artist_by_genre(id):
+    req_vars = { "apikey": apikey(),
+                 "sig": sign(),
+                 "genreids": id,
+                 "format": "json",
+                 "country": "US",
+                 "language": "en" }
+ 
+    base_url = "http://api.rovicorp.com/data/v1.1/descriptor/significantartists" 
+    r = requests.get(base_url, params=req_vars)
+    data = r.json
+    artists = data.get('artists')
+    print len(artists) + " artists"
+    f = open("genre_" + str(id) + ".json", 'w')
+    json.dump(artists, f, indent=4)
+    f.close()
 
+def cache_all_genres():
+    f = open("genre_cache.json", 'r')
+    genres = json.load(f)
+    f.close()
+    
+    for genre in genres: 
+        print 'caching ' + genre.get('name')
+        get_artist_by_genre(genre.get('id'))
+        subgenre = genre.get('subgenres')
+        for sub in subgenre:
+            print 'caching sub ' + sub.get('name')
+            get_artist_by_genre(sub.get('id'))
+
+if __name__ == "__main__":
+    print "caching genres"
+    get_genres()
+    print "caching artists"
+    cache_all_genres()
